5a604d260dc5e4e8542c5935ba8159c65a316b2f
user <user@admins-MBP-6.connectify>
2017-10-22 13:56:22 +0300

    lecture 18

---
+++ src/com/waverleysoftware/WelcomeToBank.java
@@ -0,0 +1,14 @@
+package com.waverleysoftware;
+
+public class WelcomeToBank{
+
+    public WelcomeToBank(){
+
+
+        System.out.println("Welcome to Waverley Bank");
+
+        System.out.println("We are happy to see you\n");
+
+    }
+
+}
